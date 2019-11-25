1ad8eccb0a411b4f713a71bc8985a45ed3846976
brad <brad@brad.tw>
2019-11-25 19:35:32 +0800

    4

--- app/src/main/java/tw/org/iii/android201903/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201903/MainActivity.java
@@ -3,12 +3,46 @@ package tw.org.iii.android201903;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    private String answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private String createAnswer(){
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<3){
+            set.add((int)(Math.random()*10));
+        }
+
+        LinkedList<Integer> list = new LinkedList<>();
+        for (Integer temp : set){
+            list.add(temp);
+        }
+        Collections.shuffle(list);
+
+        StringBuffer sb = new StringBuffer();
+        for (Integer temp : list){
+            sb.append(temp);
+        }
+
+        return sb.toString();
+    }
+
+    public void guess(View view) {
+        answer = createAnswer();
+        Log.v("brad", answer);
     }
 }
